232bd8f4b0241f04579e96b00bce60ab9993a95f
Peter Krokavec <peter.krokavec@sk.ibm.com>
2018-02-12 15:37:01 +0100

    -added dependency on kroky-commons-html-utils

--- src/main/java/org/kroky/html/extractor/gamebanshee/Main.java
+++ src/main/java/org/kroky/html/extractor/gamebanshee/Main.java
@@ -4,10 +4,8 @@ package org.kroky.html.extractor.gamebanshee;
  * Hello world!
  *
  */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
     }
 }
