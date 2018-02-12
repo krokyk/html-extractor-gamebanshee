5b7cc22058d0af2172c0d01e9a12a992b48308aa
Peter Krokavec <peter.krokavec@sk.ibm.com>
2018-02-12 13:58:02 +0100

    initial commit

---
+++ src/main/java/org/kroky/html/extractor/gamebanshee/Main.java
@@ -0,0 +1,13 @@
+package org.kroky.html.extractor.gamebanshee;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
